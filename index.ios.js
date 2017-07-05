a3a0357fb27115505a895455abbe2ea27e47d9fa
Paulo Gama <pgama@thoughtworks.com>
2017-07-05 17:28:18 -0300

    [CHANGE] Refactoring to avoid lint errors/warnings

--- index.ios.js
+++ index.ios.js
@@ -7,12 +7,7 @@
 import React, { Component } from 'react';
 import Onboarding from './onboarding';
 
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { AppRegistry } from 'react-native';
 
 export default class enegrecer extends Component {
   render() {
@@ -21,24 +16,5 @@ export default class enegrecer extends Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('enegrecer', () => enegrecer);
--- onboarding.js
+++ onboarding.js
@@ -1,21 +1,21 @@
-import {StyleSheet, View, Text} from 'react-native';
+import {Text, View} from 'react-native';
 import React, {Component} from 'react';
-import {PagerTabIndicator, IndicatorViewPager, PagerTitleIndicator, PagerDotIndicator} from 'rn-viewpager';
+import { IndicatorViewPager, PagerDotIndicator} from 'rn-viewpager';
 
 export default class Onboarding extends Component {
     render() {
         return (
-            <View style={{flex:1}}>
+            <View style={styles.container}>
                 <IndicatorViewPager
-                    style={{flex: 1, flexDirection: 'column'}}
+                    style={styles.indicatorViewPager}
                     indicator={this._renderDotIndicator()}>
-                    <View style={{backgroundColor:'cadetblue'}}>
+                    <View style={styles.firstView}>
                         <Text>page one</Text>
                     </View>
-                    <View style={{backgroundColor:'cornflowerblue'}}>
+                    <View style={styles.secondView}>
                         <Text>page two</Text>
                     </View>
-                    <View style={{backgroundColor:'#1AA094'}}>
+                    <View style={styles.thirdView}>
                         <Text>page three</Text>
                     </View>
                 </IndicatorViewPager>
@@ -28,3 +28,25 @@ export default class Onboarding extends Component {
     }
 
 }
+const cadetblue = 'cadetblue';
+const cornflowerblue = 'cornflowerblue';
+const green = '#1AA094';
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1
+	},
+	indicatorViewPager: {
+		flex: 1,
+		flexDirection: 'column'
+	},
+	firstView: {
+		backgroundColor: cadetblue
+	},
+	secondView: {
+		backgroundColor: cornflowerblue
+	},
+	thirdView: {
+		backgroundColor: green
+	}
+});
